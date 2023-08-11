859f64074da9465a343b5175263f619ccb5174be
Brendan A R Sechter <sgeos@hotmail.com>
2023-08-11 21:56:38 +0900

    Added tests for all errors.

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ pub mod transaction;
 pub mod types;
 pub mod utility;
 
-use {crate::utility::now, block::Block, blockchain::Blockchain, transaction::Transaction};
+use {block::Block, blockchain::Blockchain, transaction::Transaction, utility::now};
 
 #[no_mangle]
 pub extern "C" fn run() {
@@ -54,7 +54,8 @@ pub extern "C" fn run() {
         let fee = 2;
         if user_b_coins < user_a_coins {
             user_a_coins -= user_b_coins;
-        } else { // overspending
+        } else {
+            // overspending
             user_a_coins = 2 * fee;
         }
         user_a_coins -= fee;
@@ -108,17 +109,17 @@ mod tests {
     }
 
     #[test]
-    fn test_good_block() {
-        let index = 0;
-        let timestamp = now();
-        let prev_block_hash = vec![0; 32];
+    fn test_good_blockchain() {
+        let mut index = 0;
+        let mut timestamp = now();
+        let mut prev_block_hash = vec![0; 32];
         let user_a = "Alice".to_owned();
         let user_a_coins = 50;
         let user_b = "Bob".to_owned();
         let user_b_coins = 7;
         let difficulty = 0x00ff_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
 
-        let mut block = Block::new(
+        let mut genesis_block = Block::new(
             index,
             timestamp,
             prev_block_hash,
@@ -126,34 +127,148 @@ mod tests {
                 inputs: vec![],
                 outputs: vec![
                     transaction::Output {
-                        to_addr: user_a,
+                        to_addr: user_a.clone(),
                         value: user_a_coins,
                     },
                     transaction::Output {
-                        to_addr: user_b,
+                        to_addr: user_b.clone(),
                         value: user_b_coins,
                     },
                 ],
             }],
             difficulty,
         );
-        block.mine();
+        genesis_block.mine();
 
         let mut blockchain = Blockchain::new();
+        blockchain
+            .update_with_block(genesis_block)
+            .expect("Failed to add genesis block");
+
+        index += 1;
+        timestamp = now();
+        prev_block_hash = blockchain.blocks.last().unwrap().hash.clone();
+        let mut block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![
+                Transaction {
+                    inputs: vec![],
+                    outputs: vec![],
+                },
+                Transaction {
+                    inputs: blockchain.blocks[0].transactions[0].outputs.clone(),
+                    outputs: blockchain.blocks[0].transactions[0].outputs.clone(),
+                },
+            ],
+            difficulty,
+        );
+        block.mine();
+
         assert!(!blockchain.update_with_block(block).is_err());
     }
 
     #[test]
-    fn test_bad_coinbase() {
+    fn test_error_achronological_timestamp() {
+        let mut index = 0;
+        let mut timestamp = now();
+        let mut prev_block_hash = vec![0; 32];
+        let difficulty = 0x00ff_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
+
+        let mut genesis_block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![],
+            }],
+            difficulty,
+        );
+        genesis_block.mine();
+
+        let mut blockchain = Blockchain::new();
+        blockchain
+            .update_with_block(genesis_block)
+            .expect("Failed to add genesis block");
+
+        index += 1;
+        timestamp -= 1;
+        prev_block_hash = blockchain.blocks.last().unwrap().hash.clone();
+        let mut block = Block::new(index, timestamp, prev_block_hash, vec![], difficulty);
+        block.mine();
+        assert_eq!(
+            blockchain.update_with_block(block),
+            Err(AchronologicalTimestamp)
+        );
+    }
+
+    #[test]
+    fn test_error_insufficient_input_value() {
+        let mut index = 0;
+        let mut timestamp = now();
+        let mut prev_block_hash = vec![0; 32];
+        let user_a = "Alice".to_owned();
+        let user_a_coins = 50;
+        let difficulty = 0x00ff_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
+
+        let mut genesis_block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![transaction::Output {
+                    to_addr: user_a.clone(),
+                    value: user_a_coins,
+                }],
+            }],
+            difficulty,
+        );
+        genesis_block.mine();
+
+        let mut blockchain = Blockchain::new();
+        blockchain
+            .update_with_block(genesis_block)
+            .expect("Failed to add genesis block");
+
+        index += 1;
+        timestamp = now();
+        prev_block_hash = blockchain.blocks.last().unwrap().hash.clone();
+        let mut block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![
+                Transaction {
+                    inputs: vec![],
+                    outputs: vec![],
+                },
+                Transaction {
+                    inputs: vec![blockchain.blocks[0].transactions[0].outputs[0].clone()],
+                    outputs: vec![transaction::Output {
+                        to_addr: user_a.clone(),
+                        value: user_a_coins * 2,
+                    }],
+                },
+            ],
+            difficulty,
+        );
+        block.mine();
+        assert_eq!(
+            blockchain.update_with_block(block),
+            Err(InsufficientInputValue)
+        );
+    }
+
+    #[test]
+    fn test_error_invalid_coinbase_transaction() {
         let index = 0;
         let timestamp = now();
         let prev_block_hash = vec![0; 32];
         let user_a = "Alice".to_owned();
         let user_a_coins = 50;
-        let user_b = "Bob".to_owned();
-        let user_b_coins = 7;
-        let user_c = "Chris".to_owned();
-        let user_c_coins = 536;
         let difficulty = 0x00ff_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
 
         let mut block = Block::new(
@@ -162,19 +277,10 @@ mod tests {
             prev_block_hash,
             vec![Transaction {
                 inputs: vec![transaction::Output {
-                    to_addr: user_c,
-                    value: user_c_coins,
+                    to_addr: user_a,
+                    value: user_a_coins,
                 }],
-                outputs: vec![
-                    transaction::Output {
-                        to_addr: user_a,
-                        value: user_a_coins,
-                    },
-                    transaction::Output {
-                        to_addr: user_b,
-                        value: user_b_coins,
-                    },
-                ],
+                outputs: vec![],
             }],
             difficulty,
         );
@@ -184,6 +290,353 @@ mod tests {
         assert_eq!(
             blockchain.update_with_block(block),
             Err(InvalidCoinbaseTransaction)
+        );
+    }
+
+    #[test]
+    fn test_error_invalid_coinbase_transaction_fee() {
+        let mut index = 0;
+        let mut timestamp = now();
+        let mut prev_block_hash = vec![0; 32];
+        let user_a = "Alice".to_owned();
+        let user_a_coins = 50;
+        let user_b = "Bob".to_owned();
+        let user_b_coins = 7;
+        let difficulty = 0x00ff_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
+
+        let mut genesis_block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: user_a.clone(),
+                        value: user_a_coins,
+                    },
+                    transaction::Output {
+                        to_addr: user_b.clone(),
+                        value: user_b_coins,
+                    },
+                ],
+            }],
+            difficulty,
+        );
+        genesis_block.mine();
+
+        let mut blockchain = Blockchain::new();
+        blockchain
+            .update_with_block(genesis_block)
+            .expect("Failed to add genesis block");
+
+        index += 1;
+        timestamp = now();
+        prev_block_hash = blockchain.blocks.last().unwrap().hash.clone();
+        let zero_value_transaction_vector = vec![transaction::Output {
+            to_addr: user_a.clone(),
+            value: 0,
+        }];
+        let positive_value_transaction_vector =
+            vec![blockchain.blocks[0].transactions[0].outputs[0].clone()];
+        let mut block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![
+                Transaction {
+                    inputs: vec![],
+                    outputs: zero_value_transaction_vector.clone(),
+                },
+                Transaction {
+                    inputs: positive_value_transaction_vector.clone(),
+                    outputs: zero_value_transaction_vector.clone(),
+                },
+            ],
+            difficulty,
+        );
+        block.mine();
+
+        assert_eq!(
+            blockchain.update_with_block(block),
+            Err(InvalidCoinbaseTransactionFee)
+        );
+    }
+
+    #[test]
+    fn test_error_invalid_genesis_block_format() {
+        let index = 0;
+        let timestamp = now();
+        let prev_block_hash = vec![0; 32];
+        let user_a = "Alice".to_owned();
+        let user_a_coins = 50;
+        let user_b = "Bob".to_owned();
+        let user_b_coins = 7;
+        let difficulty = 0x00ff_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
+
+        let mut genesis_block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: user_a.clone(),
+                        value: user_a_coins,
+                    },
+                    transaction::Output {
+                        to_addr: user_b.clone(),
+                        value: user_b_coins,
+                    },
+                ],
+            }],
+            difficulty,
+        );
+        genesis_block.mine();
+        genesis_block.prev_block_hash = genesis_block.hash.clone();
+
+        let mut blockchain = Blockchain::new();
+        assert_eq!(
+            blockchain.update_with_block(genesis_block),
+            Err(InvalidGenesisBlockFormat)
+        );
+    }
+
+    #[test]
+    fn test_error_invalid_hash() {
+        let mut index = 0;
+        let mut timestamp = now();
+        let mut prev_block_hash = vec![0; 32];
+        let user_a = "Alice".to_owned();
+        let user_a_coins = 50;
+        let user_b = "Bob".to_owned();
+        let user_b_coins = 7;
+        let difficulty = 0x00ff_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
+
+        let mut genesis_block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: user_a.clone(),
+                        value: user_a_coins,
+                    },
+                    transaction::Output {
+                        to_addr: user_b.clone(),
+                        value: user_b_coins,
+                    },
+                ],
+            }],
+            difficulty,
+        );
+        genesis_block.mine();
+
+        let mut blockchain = Blockchain::new();
+        blockchain
+            .update_with_block(genesis_block)
+            .expect("Failed to add genesis block");
+
+        index += 1;
+        timestamp = now();
+        prev_block_hash = blockchain.blocks.last().unwrap().hash.clone();
+        let mut block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![],
+            }],
+            difficulty,
+        );
+        block.mine();
+        block.difficulty = 0;
+
+        assert_eq!(blockchain.update_with_block(block), Err(InvalidHash));
+    }
+
+    #[test]
+    fn test_error_invalid_input() {
+        let mut index = 0;
+        let mut timestamp = now();
+        let mut prev_block_hash = vec![0; 32];
+        let user_a = "Alice".to_owned();
+        let user_a_coins = 50;
+        let user_b = "Bob".to_owned();
+        let user_b_coins = 7;
+        let user_c = "Nobody".to_owned();
+        let user_c_coins = 363893;
+        let difficulty = 0x00ff_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
+
+        let mut genesis_block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: user_a.clone(),
+                        value: user_a_coins,
+                    },
+                    transaction::Output {
+                        to_addr: user_b.clone(),
+                        value: user_b_coins,
+                    },
+                ],
+            }],
+            difficulty,
+        );
+        genesis_block.mine();
+
+        let mut blockchain = Blockchain::new();
+        blockchain
+            .update_with_block(genesis_block)
+            .expect("Failed to add genesis block");
+
+        index += 1;
+        timestamp = now();
+        prev_block_hash = blockchain.blocks.last().unwrap().hash.clone();
+        let mut block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![
+                Transaction {
+                    inputs: vec![],
+                    outputs: vec![],
+                },
+                Transaction {
+                    inputs: vec![
+                        blockchain.blocks[0].transactions[0].outputs[0].clone(),
+                        transaction::Output {
+                            to_addr: user_c.clone(),
+                            value: user_c_coins,
+                        },
+                    ],
+                    outputs: vec![],
+                },
+            ],
+            difficulty,
+        );
+        block.mine();
+
+        assert_eq!(blockchain.update_with_block(block), Err(InvalidInput));
+    }
+
+    #[test]
+    fn test_error_mismatched_index() {
+        let index = 0;
+        let mut timestamp = now();
+        let mut prev_block_hash = vec![0; 32];
+        let user_a = "Alice".to_owned();
+        let user_a_coins = 50;
+        let user_b = "Bob".to_owned();
+        let user_b_coins = 7;
+        let difficulty = 0x00ff_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
+
+        let mut genesis_block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: user_a.clone(),
+                        value: user_a_coins,
+                    },
+                    transaction::Output {
+                        to_addr: user_b.clone(),
+                        value: user_b_coins,
+                    },
+                ],
+            }],
+            difficulty,
+        );
+        genesis_block.mine();
+
+        let mut blockchain = Blockchain::new();
+        blockchain
+            .update_with_block(genesis_block)
+            .expect("Failed to add genesis block");
+
+        timestamp = now();
+        prev_block_hash = blockchain.blocks.last().unwrap().hash.clone();
+        let mut block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![],
+            }],
+            difficulty,
+        );
+        block.mine();
+        assert_eq!(blockchain.update_with_block(block), Err(MismatchedIndex));
+    }
+
+    #[test]
+    fn test_error_mismatched_previous_hash() {
+        let mut index = 0;
+        let mut timestamp = now();
+        let mut prev_block_hash = vec![0; 32];
+        let user_a = "Alice".to_owned();
+        let user_a_coins = 50;
+        let user_b = "Bob".to_owned();
+        let user_b_coins = 7;
+        let difficulty = 0x00ff_ffff_ffff_ffff_ffff_ffff_ffff_ffff;
+
+        let mut genesis_block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![
+                    transaction::Output {
+                        to_addr: user_a.clone(),
+                        value: user_a_coins,
+                    },
+                    transaction::Output {
+                        to_addr: user_b.clone(),
+                        value: user_b_coins,
+                    },
+                ],
+            }],
+            difficulty,
+        );
+        genesis_block.mine();
+
+        let mut blockchain = Blockchain::new();
+        blockchain
+            .update_with_block(genesis_block)
+            .expect("Failed to add genesis block");
+
+        index += 1;
+        timestamp = now();
+        prev_block_hash = blockchain.blocks.last().unwrap().hash.clone();
+        prev_block_hash[0] += 1;
+        let mut block = Block::new(
+            index,
+            timestamp,
+            prev_block_hash,
+            vec![Transaction {
+                inputs: vec![],
+                outputs: vec![],
+            }],
+            difficulty,
+        );
+        block.mine();
+
+        assert_eq!(
+            blockchain.update_with_block(block),
+            Err(MismatchedPreviousHash)
         );
     }
 }
